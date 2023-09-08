1273c08d215dc8471dd5e339a43fc7768239bf56
sebastianmartig <sebastianmartig@gmail.com>
2023-09-08 09:30:32 -0300

    data models & ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  resources :books
+  root 'pages#index'
   get 'pages/index'
+  get 'pages/about'
+  get 'pages/cart'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
---
+++ db/migrate/20230907185513_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :books do |t|
+      t.string :name
+      t.string :author
+      t.text :description
+      t.string :isbn
+      t.string :publisher
+
+      t.timestamps
+    end
+  end
+end
